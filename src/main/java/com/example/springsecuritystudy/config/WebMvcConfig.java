ca3f8eb8e3d5ef73824a475f9ed9b42ddc2d405b
LillLEE <kopqwe147@gmail.com>
2021-11-15 20:30:11 +0900

    환경설정 셋팅

---
+++ src/main/java/com/example/springsecuritystudy/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.example.springsecuritystudy.config;
+
+
+import org.springframework.boot.web.servlet.view.MustacheViewResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        MustacheViewResolver mustacheViewResolver = new MustacheViewResolver();
+        mustacheViewResolver.setCharset("UTF-8");
+        mustacheViewResolver.setContentType("text/html; charset=UTF-8");
+        mustacheViewResolver.setPrefix("classpath:/templates/");
+        mustacheViewResolver.setPrefix(".html");
+
+        registry.viewResolver(mustacheViewResolver);
+    }
+}
